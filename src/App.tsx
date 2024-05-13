722caa1076836004a423fe3a1b68c6fa9f228a42
farialimpa <farialimpa57525@gmail.com>
2024-05-14 03:38:51 +0600

    final-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Message from './Message';
 import ListGroup from './components/ListGroup';
+// import { useState } from 'react';
+import Alert from './components/Alert';
+import Button from './components/Button';
 
 const App = () => {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Japan"];
-  const handleSelectItem = (item: string)=> {
-    console.log(item);
-  };
-
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
       {/* <Message></Message> */}
-      <ListGroup items = {items} heading = "Cities" onSelectItem = {handleSelectItem}></ListGroup>
+      {/* <ListGroup></ListGroup> */}
+      {/* <Alert children = "Hello World"></Alert> */}
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color= "primary" onClick = {() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 };
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
 
-const Alert = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Alert = ({ children, onClose}: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
