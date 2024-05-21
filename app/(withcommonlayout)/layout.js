a865199179cd7cbceed7807d500d47e04b48bf63
farialimpa <farialimpa57525@gmail.com>
2024-05-21 23:53:09 +0600

    header section

--- app/(withcommonlayout)/layout.js
+++ app/(withcommonlayout)/layout.js
@@ -3,8 +3,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import NavbarHome from "../components/home/NavbarHome";
 import FooterHome from "../components/home/FooterHome";
-import "../css/globals.scss";
-import "../css/globals.scss";
+import "../globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +11,7 @@ const layout = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-black">
           <NavbarHome></NavbarHome>
           {children}
           <FooterHome></FooterHome>
