038fcba11fbc283e318d66990d7df7e4e31300af
kolinabir <knkolin9@gmail.com>
2024-05-22 11:36:18 +0600

    updated

--- app/components/home/NavbarHome.jsx
+++ app/components/home/NavbarHome.jsx
@@ -7,8 +7,8 @@ const NavbarHome = () => {
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
+          <Link
+            href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <Image
@@ -21,7 +21,7 @@ const NavbarHome = () => {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               PortfolioMaker
             </span>
-          </a>
+          </Link>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
@@ -49,13 +49,13 @@ const NavbarHome = () => {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/"
                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
                 <a
